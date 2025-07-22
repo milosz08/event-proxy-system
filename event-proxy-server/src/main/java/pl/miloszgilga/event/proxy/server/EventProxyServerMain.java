9299fb2386cde18c4eb6fd978276dc99796762e3
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-07-22 09:17:55 +0200

    rewrite main class and add shutdown hook

--- event-proxy-server/src/main/java/pl/miloszgilga/event/proxy/server/EventProxyServerMain.java
+++ event-proxy-server/src/main/java/pl/miloszgilga/event/proxy/server/EventProxyServerMain.java
@@ -1,11 +1,28 @@
 package pl.miloszgilga.event.proxy.server;
 
-class EventProxyServerMain {
-  public static void main(String[] args) {
-    final HttpProxyServerThread httpProxyServerThread = new HttpProxyServerThread(4365);
-    final SmtpProxyServerThread smtpProxyServerThread = new SmtpProxyServerThread(1025, 10);
+class EventProxyServerMain implements Runnable {
+  private final HttpProxyServerThread httpProxyServerThread;
+  private final SmtpProxyServerThread smtpProxyServerThread;
 
+  EventProxyServerMain() {
+    this.httpProxyServerThread = new HttpProxyServerThread(4365);
+    this.smtpProxyServerThread = new SmtpProxyServerThread(1025, 10);
+  }
+
+  public static void main(String[] args) {
+    final EventProxyServerMain main = new EventProxyServerMain();
+    Runtime.getRuntime().addShutdownHook(new Thread(main));
+    main.start();
+  }
+
+  void start() {
     httpProxyServerThread.start();
     smtpProxyServerThread.start();
+  }
+
+  @Override
+  public void run() {
+    httpProxyServerThread.stop();
+    smtpProxyServerThread.stop();
   }
 }
