2f963ae4b60c457b29ca89d46b9d388e391b54cf
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-09-04 20:09:57 +0200

    remove unused enum field

--- event-proxy-server/src/main/java/pl/miloszgilga/event/proxy/server/FieldType.java
+++ event-proxy-server/src/main/java/pl/miloszgilga/event/proxy/server/FieldType.java
@@ -5,22 +5,20 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 enum FieldType {
-  TEXT("TEXT", (ps, i, value) -> ps.setString(i, (String) value)),
-  INTEGER("INTEGER", (ps, i, value) -> ps.setInt(i, (Integer) value)),
-  REAL("REAL", (ps, i, value) -> ps.setDouble(i, (Double) value)),
-  NUMERIC("NUMERIC", (ps, i, value) -> ps.setBigDecimal(i, (BigDecimal) value)),
+  TEXT((ps, i, value) -> ps.setString(i, (String) value)),
+  INTEGER((ps, i, value) -> ps.setInt(i, (Integer) value)),
+  REAL((ps, i, value) -> ps.setDouble(i, (Double) value)),
+  NUMERIC((ps, i, value) -> ps.setBigDecimal(i, (BigDecimal) value)),
   ;
 
-  private final String sqliteType;
   private final StatementConsumer statementConsumer;
 
-  FieldType(String sqliteType, StatementConsumer statementConsumer) {
-    this.sqliteType = sqliteType;
+  FieldType(StatementConsumer statementConsumer) {
     this.statementConsumer = statementConsumer;
   }
 
   String getSqliteType() {
-    return sqliteType;
+    return name();
   }
 
   // fields must be mapped to specialized methods, because SQLite driver not implements setObject
