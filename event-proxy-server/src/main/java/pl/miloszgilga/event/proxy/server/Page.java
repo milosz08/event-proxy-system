447de5446b063800d0ee89032374a0a37b5c9bda
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-09-05 20:53:50 +0200

    minor changes

--- event-proxy-server/src/main/java/pl/miloszgilga/event/proxy/server/Page.java
+++ event-proxy-server/src/main/java/pl/miloszgilga/event/proxy/server/Page.java
@@ -2,9 +2,5 @@ package pl.miloszgilga.event.proxy.server;
 
 import java.util.List;
 
-public record Page<T>(
-  List<T> elements,
-  boolean hasNext,
-  long totalElements
-) {
+public record Page<T>(List<T> elements, boolean hasNext, long totalElements) {
 }
