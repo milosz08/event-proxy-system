6efa3712321168d8309520c56efb84b072590192
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-10-20 03:46:49 +0200

    add possibility to load properties directly from env variables (for docker image)

--- event-proxy-server/docker/README.md
+++ event-proxy-server/docker/README.md
@@ -23,6 +23,20 @@ docker run -d \
   -p 8080:8080 \
   -e EVENT_PROXY_SERVER_XMS=128m \
   -e EVENT_PROXY_SERVER_XMX=128m \
+  -e HTTP_PORT=4365 \
+  -e SSE_HEARTBEAT_INTERVAL_SEC=10 \
+  -e SSE_HANDSHAKE_PENDING_SEC=180 \
+  -e SMTP_PORT=4366 \
+  -e SMTP_THREAD_POOL_SIZE=10 \
+  -e SMTP_QUEUE_CAPACITY=10 \
+  -e SMTP_SENDER_SUFFIX=event-proxy-system \
+  -e DB_PATH=events.db \
+  -e DB_POOL_SIZE=5 \
+  -e SESSION_TTL_SEC=7200 \
+  -e SESSION_CLEAR_INTERVAL_SEC=3600 \
+  -e ACCOUNT_USERNAME=admin \
+  -e ACCOUNT_PASSWORD_LENGTH=20 \
+  -e ACCOUNT_PASSWORD_HASH_STRENGTH=10 \
   milosz08/event-proxy-server:latest
 ```
 
@@ -38,6 +52,24 @@ services:
     environment:
       EVENT_PROXY_SERVER_XMS: 128m
       EVENT_PROXY_SERVER_XMX: 128m
+      # http and smtp servers
+      HTTP_PORT: 4365
+      SSE_HEARTBEAT_INTERVAL_SEC: 10
+      SSE_HANDSHAKE_PENDING_SEC: 180
+      SMTP_PORT: 4366
+      SMTP_THREAD_POOL_SIZE: 10
+      SMTP_QUEUE_CAPACITY: 10
+      SMTP_SENDER_SUFFIX: event-proxy-system
+      # database
+      DB_PATH: events.db
+      DB_POOL_SIZE: 5
+      # session
+      SESSION_TTL_SEC: 7200
+      SESSION_CLEAR_INTERVAL_SEC: 3600
+      # account
+      ACCOUNT_USERNAME: admin
+      ACCOUNT_PASSWORD_LENGTH: 20
+      ACCOUNT_PASSWORD_HASH_STRENGTH: 10
     networks:
       - event-proxy-server-network
 
--- event-proxy-server/src/main/java/pl/miloszgilga/event/proxy/server/AppConfig.java
+++ event-proxy-server/src/main/java/pl/miloszgilga/event/proxy/server/AppConfig.java
@@ -1,11 +1,15 @@
 package pl.miloszgilga.event.proxy.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class AppConfig {
+  private static final Logger LOG = LoggerFactory.getLogger(AppConfig.class);
   private static final String PROPERTIES_FILE = "server.properties";
 
   private final Properties properties;
@@ -15,22 +19,42 @@ public class AppConfig {
     try (final InputStream input = new FileInputStream(PROPERTIES_FILE)) {
       properties.load(input);
     } catch (IOException ex) {
-      throw new RuntimeException(String.format("unable to load %s file", PROPERTIES_FILE), ex);
+      LOG.warn("Unable to load file: {}. Configuration will rely solely on environment variables.",
+        PROPERTIES_FILE);
     }
   }
 
+  private String resolveProperty(AppConfig.Prop prop) {
+    final String envKey = prop.name();
+    final String envValue = System.getenv(envKey);
+
+    if (envValue != null && !envValue.isEmpty()) {
+      return envValue;
+    }
+    final String propKey = prop.key;
+    final String propValue = properties.getProperty(propKey);
+
+    if (propValue == null) {
+      LOG.warn("Configuration key not found either in environment variables (as: {}) or in the " +
+        "properties file (as: {}).", envKey, propKey);
+    }
+    return propValue;
+  }
+
   public String getAsStr(AppConfig.Prop prop) {
-    return properties.getProperty(prop.key);
+    return resolveProperty(prop);
   }
 
   public long getAsLong(AppConfig.Prop prop) {
-    return Utils.safetyParseLong(properties.getProperty(prop.key), 0L);
+    return Utils.safetyParseLong(resolveProperty(prop), 0L);
   }
 
   public int getAsInt(AppConfig.Prop prop) {
-    return Utils.safetyParseInt(properties.getProperty(prop.key), 0);
+    return Utils.safetyParseInt(resolveProperty(prop), 0);
   }
 
+  // prop.name() -> key to environment path (ex. HTTP_PORT)
+  // prop.key -> key to property in properties file (ex. http-port)
   public enum Prop {
     HTTP_PORT("http-port"),
     SSE_HEARTBEAT_INTERVAL_SEC("sse-heartbeat-interval-sec"),
