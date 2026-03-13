e873a55c3d482e51e5eb44173478a1ad48ae739f
Miłosz Gilga <gilgamilosz451@gmail.com>
2026-03-13 19:34:30 +0100

    creates missing database parent directory on initialization

--- event-proxy-server/src/main/java/pl/miloszgilga/event/proxy/server/db/DbConnectionPool.java
+++ event-proxy-server/src/main/java/pl/miloszgilga/event/proxy/server/db/DbConnectionPool.java
@@ -2,16 +2,21 @@ package pl.miloszgilga.event.proxy.server.db;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DbConnectionPool {
-  private final HikariDataSource dataSource;
+  private static final Logger LOG = LoggerFactory.getLogger(DbConnectionPool.class);
 
   private volatile static DbConnectionPool instance;
+  private final HikariDataSource dataSource;
 
   private DbConnectionPool(String dbName, int maximumPoolSize) {
+    createMissingDir(dbName);
     final HikariConfig config = new HikariConfig();
 
     config.setJdbcUrl("jdbc:sqlite:" + dbName);
@@ -24,10 +29,6 @@ public class DbConnectionPool {
     dataSource = new HikariDataSource(config);
   }
 
-  public Connection getConnection() throws SQLException {
-    return dataSource.getConnection();
-  }
-
   public static DbConnectionPool getInstance(String dbName, int maximumPoolSize) {
     if (instance == null) {
       synchronized (DbConnectionPool.class) {
@@ -37,5 +38,19 @@ public class DbConnectionPool {
       }
     }
     return instance;
+  }
+
+  private void createMissingDir(String dbName) {
+    final File dbFile = new File(dbName);
+    final File parentDir = dbFile.getParentFile();
+    if (parentDir != null && !parentDir.exists()) {
+      if (parentDir.mkdirs()) {
+        LOG.info("Created missing directory structure: {}", parentDir.getPath());
+      }
+    }
+  }
+
+  public Connection getConnection() throws SQLException {
+    return dataSource.getConnection();
   }
 }
